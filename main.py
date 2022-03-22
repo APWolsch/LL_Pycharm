bd1a0ac7db2f25dcbf33b85464ab56f86794e6f7
Andrew Wolschlag <apwolsch@ncsu.edu>
2022-03-21 23:06:58 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+
+# Document to learn Pycharm as well as keyboard shortcuts
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Keyboard shortcuts
+# ctrl + w select larger sections of code
+# shift + arrows = select character into word
+# ctrl + alt + shift + j = select a word and type in multiple places
+# ctrl + = d duplicates code
+# ctrl + / = comment section
+# ctrl + shift up or down moves entire lines
+
+# ctrl + g select specific line
+# ctrl + q documentation for function
+# ctrl + i application of code
+
+# right click + refactor = replace all instances with something else
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print('How are you doing')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')

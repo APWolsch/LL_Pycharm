f78844b6cebce426bd9a7b87c1d7881033de26fc
Andrew Wolschlag <apwolsch@ncsu.edu>
2022-03-21 23:12:00 -0400

    first addition

--- main.py
+++ main.py
@@ -31,3 +31,5 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+# Eventually figure out how to work with different branches
